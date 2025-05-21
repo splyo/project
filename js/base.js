6cc9b657a2e5dd87dde5c4c22babb42121eed00a
splyo <king.llama.2004@gmail.com>
2025-05-21 11:16:29 +0200

    kontaktok hozzáadása a db-hez

--- js/base.js
+++ js/base.js
@@ -5,7 +5,6 @@ xhr.open("GET", "http://localhost:3000/contents", true)
 xhr.onload = function() {
     let html = "";
     let test = JSON.parse(xhr.responseText);
-    let db = test.length
     test.forEach(element => {
         html += `<table><td>${element.nev}</td><td>${element.szam}</td></table>`
     })
@@ -14,6 +13,14 @@ xhr.onload = function() {
 
 xhr.send()
 
+function Save() {
+    let data = {}
+    data.nev = document.getElementById("u_nev").value
+    data.szam = document.getElementById("u_text").value
+    xhr.open("POST", "http://localhost:3000/contents", true)
+    xhr.send(JSON.stringify(data))
+
+}
 
 //let conts = new Array();
 
